e0df40cd40f3c84be58fdfffaf0e010a4003e81f
Fabricio Borgobello <fborgobello@gmail.com>
2024-02-07 16:21:33 +0100

    Add max width

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import { List } from '@/components/List';
 
 export default function App() {
   return (
-    <div className="flex flex-col gap-10 p-4">
+    <div className='flex flex-col gap-10 p-4 max-w-2xl mx-auto'>
       <div>
-        <h1 className="text-4xl text-center">Supabase Test</h1>
-        <p className="text-center text-gray-500">
+        <h1 className='text-4xl text-center'>Supabase Test</h1>
+        <p className='text-center text-gray-500'>
           This is a test project to try out Supabase
         </p>
       </div>
